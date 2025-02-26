81a5f51f5203a34225f1b6d0201048e65e26f1ff
nikakaladze <nikakaladze89@gmail.com>
2025-02-27 00:07:55 +0400

    chore:create @Input and pass data child from parent

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChildComponent } from "./child/child.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChildComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
 
+  messages:string[] = ['first','second'];
   
 
 }
