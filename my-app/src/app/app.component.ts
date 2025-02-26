8737527d0f4cb8aed2fae73bec964797a6188a8f
nikakaladze <nikakaladze89@gmail.com>
2025-02-27 00:57:35 +0400

    chore:Add property and attribute binding in elements

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,34 +1,40 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ChildComponent } from "./child/child.component";
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ChildComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit, OnDestroy{
-title:string = 'fomi';
-count:number = 0;
-  public increament():void{
+export class AppComponent implements OnInit, OnDestroy {
+  title: string = 'fomi';
+  count: number = 0;
+  public increament(): void {
     this.count++;
-
   }
-  public deacriment():void{
-    this.count -=1;
-
+  public deacriment(): void {
+    this.count -= 1;
   }
 
-  messages:string[] = ['first','second'];
-
+  messages: string[] = ['first', 'second'];
 
   public ngOnInit(): void {
     console.log('კომპონენტი ინიციალიზდა');
-    
   }
   public ngOnDestroy(): void {
     console.log('კომპონენტი განადგურდა');
-    
+  }
+  imgData = {
+    src: 'https://angular.io/assets/images/logos/angular/angular.svg',
+    alt: 'Angular Logo',
+  };
+  btnDisabled = true;
+  squareClass = 'square-red';
+
+  changeSquare(): void {
+    this.squareClass =
+      this.squareClass === 'square-red' ? 'square-blue' : 'square-red';
   }
 }
